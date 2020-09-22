85e882a307a3809500e0cfd01c1e64577732f8ef
Aravindkumar <aravind@gmail.com>
2020-09-22 19:28:12 +0530

    refs jenkins sample

---
+++ JenkinsApp.java
@@ -0,0 +1,18 @@
+
+public class JenkinsApp{
+
+	public static void main(String[] args) {
+		System.out.println("******************");
+		System.out.println("*********JENKINS JOB SAMPLE***");
+		System.out.println("******************");
+
+	}
+
+}
+
+
+
+
+
+
+

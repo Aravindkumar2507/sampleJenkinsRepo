0dc858658ba6ea9f66291d172a58241d1542bafa
Aravindkumar2507 <69254706+Aravindkumar2507@users.noreply.github.com>
2020-09-22 23:55:05 +0530

    update web hook sample

--- JenkinsApp.java
+++ JenkinsApp.java
@@ -3,7 +3,7 @@ public class JenkinsApp{
 
 	public static void main(String[] args) {
 		System.out.println("******************");
-		System.out.println("*********JENKINS JOB TRIGGER BUILD by WEB Hook When Content Changes***");
+		System.out.println("*********JENKINS Build JOB TRIGGER BUILD by WEB Hook  When Content Changes***");
 		System.out.println("******************");
 
 	}
